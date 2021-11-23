bfc8906228cdbdfa56aa51d75740c627bfe61e24
hootoon <61035108+hootoon@users.noreply.github.com>
2021-11-23 15:48:28 +0800

    Update train_l.py

--- network/train_l.py
+++ network/train_l.py
@@ -122,135 +122,136 @@ def main(argv=None):
     ###### Training ######
     for epoch in range(opt.epoch, opt.n_epochs):
         for i, batch in enumerate(dataloader):
-            # Set model input
-            real_A = Variable(input_A.copy_(batch['A']))
-            real_B = Variable(input_B.copy_(batch['B']))
-            mask_real = Variable(input_C.copy_(batch['C']))
+                # Set model input
+                real_A = Variable(input_A.copy_(batch['A']))
+                real_B = Variable(input_B.copy_(batch['B']))
+                mask_real = Variable(input_C.copy_(batch['C']))
 
-            ###### Generators A2B and B2A ######
-            optimizer_G.zero_grad()
+                ###### Generators A2B and B2A ######
+                optimizer_G.zero_grad()
 
-            # Identity loss
-            # G_A2B(B) should equal B if real B is fed
-            same_B = netG_A2B(real_B)
-            loss_identity_B = criterion_identity(same_B, real_B) * 5.0  # ||Gb(b)-b||1
-            # G_B2A(A) should equal A if real A is fed, so the mask should be all zeros
-            same_A = netG_B2A(real_A, mask_non_highlight)
-            loss_identity_A = criterion_identity(same_A, real_A) * 5.0  # ||Ga(a)-a||1
+                # Identity loss
+                # G_A2B(B) should equal B if real B is fed
+                same_B = netG_A2B(real_B,mask_non_highlight)
+                loss_identity_B = criterion_identity(same_B, real_B) * 5.0  # ||Gb(b)-b||1
+                # G_B2A(A) should equal A if real A is fed, so the mask should be all zeros
+                same_A = netG_B2A(real_A, mask_non_highlight)
+                loss_identity_A = criterion_identity(same_A, real_A) * 5.0  # ||Ga(a)-a||1
 
-            # GAN loss
-            fake_B = netG_A2B(real_A)
-            pred_fake = netD_B(fake_B)
-            loss_GAN_A2B = criterion_GAN(pred_fake, target_real)  # log(Db(Gb(a)))
+                # GAN loss
+                fake_B = netG_A2B(real_A,mask_real)
+                pred_fake = netD_B(fake_B)
+                loss_GAN_A2B = criterion_GAN(pred_fake, target_real)  # log(Db(Gb(a)))
 
-            mask = mask_real
-            mask_queue.insert(mask)
+                mask = mask_real
+                mask_queue.insert(mask)
 
-            fake_A = netG_B2A(real_B, mask_queue.rand_item())
-            pred_fake = netD_A(fake_A)
-            loss_GAN_B2A = criterion_GAN(pred_fake, target_real)  # log(Da(Ga(b)))
+                mask_random = mask_queue.rand_item()
+                fake_A = netG_B2A(real_B, mask_random)
+                pred_fake = netD_A(fake_A)
+                loss_GAN_B2A = criterion_GAN(pred_fake, target_real)  # log(Da(Ga(b)))
 
-            # Cycle loss
-            recovered_A = netG_B2A(fake_B, mask_queue.last_item())  # real highlight, false highlight free
-            loss_cycle_ABA = criterion_cycle(recovered_A, real_A) * 10.0  # ||Ga(Gb(a))-a||1
+                # Cycle loss
+                recovered_A = netG_B2A(fake_B, mask_queue.last_item())  # real highlight, false highlight free
+                loss_cycle_ABA = criterion_cycle(recovered_A, real_A) * 10.0  # ||Ga(Gb(a))-a||1
 
-            recovered_B = netG_A2B(fake_A)
-            loss_cycle_BAB = criterion_cycle(recovered_B, real_B) * 10.0  # ||Gb(Ga(b))-b||1
+                recovered_B = netG_A2B(fake_A,mask_random)
+                loss_cycle_BAB = criterion_cycle(recovered_B, real_B) * 10.0  # ||Gb(Ga(b))-b||1
 
-            # Total loss
-            loss_G = loss_identity_A + loss_identity_B + loss_GAN_A2B + loss_GAN_B2A + loss_cycle_ABA + loss_cycle_BAB
-            loss_G.backward()
+                # Total loss
+                loss_G = loss_identity_A + loss_identity_B + loss_GAN_A2B + loss_GAN_B2A + loss_cycle_ABA + loss_cycle_BAB
+                loss_G.backward()
 
-            # G_losses.append(loss_G.item())
-            G_losses_temp += loss_G.item()
+                # G_losses.append(loss_G.item())
+                G_losses_temp += loss_G.item()
 
-            optimizer_G.step()
-            ###################################
+                optimizer_G.step()
+                ###################################
 
-            ###### Discriminator A ######
-            optimizer_D_A.zero_grad()
-            # Real loss
-            pred_real = netD_A(real_A)
-            loss_D_real = criterion_GAN(pred_real, target_real)  # log(Da(a))
+                ###### Discriminator A ######
+                optimizer_D_A.zero_grad()
+                # Real loss
+                pred_real = netD_A(real_A)
+                loss_D_real = criterion_GAN(pred_real, target_real)  # log(Da(a))
 
-            # Fake loss
-            fake_A = fake_A_buffer.push_and_pop(fake_A)
-            pred_fake = netD_A(fake_A.detach())
-            loss_D_fake = criterion_GAN(pred_fake, target_fake)  # log(1-Da(G(b)))
+                # Fake loss
+                fake_A = fake_A_buffer.push_and_pop(fake_A)
+                pred_fake = netD_A(fake_A.detach())
+                loss_D_fake = criterion_GAN(pred_fake, target_fake)  # log(1-Da(G(b)))
 
-            # Total loss
-            loss_D_A = (loss_D_real + loss_D_fake) * 0.5
-            loss_D_A.backward()
+                # Total loss
+                loss_D_A = (loss_D_real + loss_D_fake) * 0.5
+                loss_D_A.backward()
 
-            # D_A_losses.append(loss_D_A.item())
-            D_A_losses_temp += loss_D_A.item()
+                # D_A_losses.append(loss_D_A.item())
+                D_A_losses_temp += loss_D_A.item()
 
-            optimizer_D_A.step()
-            ###################################
+                optimizer_D_A.step()
+                ###################################
 
-            ###### Discriminator B ######
-            optimizer_D_B.zero_grad()
+                ###### Discriminator B ######
+                optimizer_D_B.zero_grad()
 
-            # Real loss
-            pred_real = netD_B(real_B)
-            loss_D_real = criterion_GAN(pred_real, target_real)  # log(Db(b))
+                # Real loss
+                pred_real = netD_B(real_B)
+                loss_D_real = criterion_GAN(pred_real, target_real)  # log(Db(b))
 
-            # Fake loss
-            fake_B = fake_B_buffer.push_and_pop(fake_B)
-            pred_fake = netD_B(fake_B.detach())
-            loss_D_fake = criterion_GAN(pred_fake, target_fake)  # log(1-Db(G(a)))
+                # Fake loss
+                fake_B = fake_B_buffer.push_and_pop(fake_B)
+                pred_fake = netD_B(fake_B.detach())
+                loss_D_fake = criterion_GAN(pred_fake, target_fake)  # log(1-Db(G(a)))
 
-            # Total loss
-            loss_D_B = (loss_D_real + loss_D_fake) * 0.5
-            loss_D_B.backward()
+                # Total loss
+                loss_D_B = (loss_D_real + loss_D_fake) * 0.5
+                loss_D_B.backward()
 
-            # D_B_losses.append(loss_D_B.item())
-            D_B_losses_temp += loss_D_B.item()
+                # D_B_losses.append(loss_D_B.item())
+                D_B_losses_temp += loss_D_B.item()
 
-            optimizer_D_B.step()
-            ###################################
+                optimizer_D_B.step()
+                ###################################
 
-            curr_iter += 1
+                curr_iter += 1
 
-            if (i + 1) % opt.iter_loss == 0:
-                log = 'Epoch: %d, [iter %d], [loss_G %.5f], [loss_G_identity %.5f], [loss_G_GAN %.5f],' \
-                      '[loss_G_cycle %.5f], [loss_D %.5f]' % \
-                      (epoch, curr_iter, loss_G, (loss_identity_A + loss_identity_B), (loss_GAN_A2B + loss_GAN_B2A),
-                       (loss_cycle_ABA + loss_cycle_BAB), (loss_D_A + loss_D_B))
-                print(log)
-                open(opt.log_path, 'a').write(log + '\n')
+                if (i + 1) % iter_loss == 0:
+                    log = 'Epoch: %d, [iter %d], [loss_G %.5f], [loss_G_identity %.5f], [loss_G_GAN %.5f],' \
+                          '[loss_G_cycle %.5f], [loss_D %.5f]' % \
+                          (epoch, curr_iter, loss_G, (loss_identity_A + loss_identity_B), (loss_GAN_A2B + loss_GAN_B2A),
+                           (loss_cycle_ABA + loss_cycle_BAB), (loss_D_A + loss_D_B))
+                    print(log)
+                    open(log_path, 'a').write(log + '\n')
 
-                G_losses.append(G_losses_temp / opt.iter_loss)
-                D_A_losses.append(D_A_losses_temp / opt.iter_loss)
-                D_B_losses.append(D_B_losses_temp / opt.iter_loss)
-                G_losses_temp = 0
-                D_A_losses_temp = 0
-                D_B_losses_temp = 0
+                    G_losses.append(G_losses_temp / iter_loss)
+                    D_A_losses.append(D_A_losses_temp / iter_loss)
+                    D_B_losses.append(D_B_losses_temp / iter_loss)
+                    G_losses_temp = 0
+                    D_A_losses_temp = 0
+                    D_B_losses_temp = 0
 
-                avg_log = '[the last %d iters], [loss_G %.5f], [D_A_losses %.5f], [D_B_losses %.5f],' \
-                          % (opt.iter_loss, G_losses[G_losses.__len__() - 1], D_A_losses[D_A_losses.__len__() - 1], \
-                             D_B_losses[D_B_losses.__len__() - 1])
-                print(avg_log)
-                open(opt.log_path, 'a').write(avg_log + '\n')
+                    avg_log = '[the last %d iters], [loss_G %.5f], [D_A_losses %.5f], [D_B_losses %.5f],' \
+                              % (iter_loss, G_losses[G_losses.__len__() - 1], D_A_losses[D_A_losses.__len__() - 1], \
+                                 D_B_losses[D_B_losses.__len__() - 1])
+                    print(avg_log)
+                    open(log_path, 'a').write(avg_log + '\n')
 
-        # Update learning rates
-        lr_scheduler_G.step()
-        lr_scheduler_D_A.step()
-        lr_scheduler_D_B.step()
+            # Update learning rates
+            lr_scheduler_G.step()
+            lr_scheduler_D_A.step()
+            lr_scheduler_D_B.step()
 
-        # Save models checkpoints
-        torch.save(netG_A2B.state_dict(), 'model_l/netG_A2B.pth')
-        torch.save(netG_B2A.state_dict(), 'model_l/netG_B2A.pth')
-        torch.save(netD_A.state_dict(), 'model_l/netD_A.pth')
-        torch.save(netD_B.state_dict(), 'model_l/netD_B.pth')
+            # Save models checkpoints
+            torch.save(netG_A2B.state_dict(), 'model_l/netG_A2B.pth')
+            torch.save(netG_B2A.state_dict(), 'model_l/netG_B2A.pth')
+            torch.save(netD_A.state_dict(), 'model_l/netD_A.pth')
+            torch.save(netD_B.state_dict(), 'model_l/netD_B.pth')
 
-        if (epoch + 1) % opt.snapshot_epochs == 0:
-            torch.save(netG_A2B.state_dict(), ('model_l/netG_A2B_%d.pth' % (epoch + 1)))
-            torch.save(netG_B2A.state_dict(), ('model_l/netG_B2A_%d.pth' % (epoch + 1)))
-            torch.save(netD_A.state_dict(), ('model_l/netD_A_%d.pth' % (epoch + 1)))
-            torch.save(netD_B.state_dict(), ('model_l/netD_B_%d.pth' % (epoch + 1)))
+            if (epoch + 1) % snapshot_epochs == 0:
+                torch.save(netG_A2B.state_dict(), ('model_l/netG_A2B_%d.pth' % (epoch + 1)))
+                torch.save(netG_B2A.state_dict(), ('model_l/netG_B2A_%d.pth' % (epoch + 1)))
+                torch.save(netD_A.state_dict(), ('model_l/netD_A_%d.pth' % (epoch + 1)))
+                torch.save(netD_B.state_dict(), ('model_l/netD_B_%d.pth' % (epoch + 1)))
 
-        print('Epoch:{}'.format(epoch))
+            print('Epoch:{}'.format(epoch))
 
 
 if __name__ == '__main__':
